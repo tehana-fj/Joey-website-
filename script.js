5a904aa4fce183ba8a88e9bcb699e9d8fa51c955
tehana-fj <josephineladdpeter@gmail.com>
2025-06-02 14:58:48 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("Joey's website is loaded with ✨!");
+  console.log("Joey's site loaded with super powers ⚡");
+
+  const toggle = document.getElementById('dark-toggle');
+  if (toggle) {
+    toggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+    });
+  }
 });
